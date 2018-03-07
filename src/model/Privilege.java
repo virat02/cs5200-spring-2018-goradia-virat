3b663a50a521bab8308f23617c20fc06989ca591
virat02 <goradia.v@husky.neu.edu>
2018-03-07 14:34:39 -0500

    HW3 submission ready

--- src/model/Privilege.java
+++ src/model/Privilege.java
@@ -4,22 +4,14 @@ public class Privilege {
 	
 	public Privilege() {
 		super();
-	}
-	
-	public Privilege(String privilegeDescription) {
-		super();
-		this.privilegeDescription = privilegeDescription;
-	}
-	
+	}	
 
-	public Privilege(int id, String privilegeDescription) {
+	public Privilege(int id) {
 		super();
 		this.id = id;
-		this.privilegeDescription = privilegeDescription;
 	}
 
 	private int id;
-	private String privilegeDescription;
 	
 	public int getId() {
 		return id;
@@ -27,14 +19,6 @@ public class Privilege {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public String getPrivilegeDescription() {
-		return privilegeDescription;
-	}
-
-	public void setPrivilegeDescription(String privilegeDescription) {
-		this.privilegeDescription = privilegeDescription;
 	}
 	
 }
